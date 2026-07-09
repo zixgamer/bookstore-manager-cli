08fb5811ce3226c2e12fe46273fc55b9ffd672e8
zixgamer <vitkoshi@gmail.com>
2026-07-10 00:51:52 +0200

    feat: implementação de AutorController e AutorMenu

---
+++ src/controllers/AutorController.ts
@@ -0,0 +1,83 @@
+import { AutorService } from "../services/AutorService";
+
+export class AutorController {
+  constructor(private autorService: AutorService) {}
+
+  async criar(nomeRaw: string): Promise<string> {
+    try {
+      const novoAutor = await this.autorService.criar({ nome: nomeRaw });
+      return `Autor: ${novoAutor.id} | Nome: ${novoAutor.nome} foi cadastrado com sucesso!! `;
+    } catch (error) {
+      return `Erro ao criar autor:${(error as Error).message}`;
+    }
+  }
+
+  async listarTodos(): Promise<string> {
+    try {
+      const autores = await this.autorService.listarTodos();
+
+      if (autores.length === 0) {
+        return `Nenhum autor cadastrado no sistema`;
+      }
+
+      let tabela = "====================================";
+      tabela += "         LISTA DE AUTORES           ";
+      tabela += "====================================";
+      tabela += "ID| Nome";
+      tabela += "------------------------------------";
+
+      autores.forEach((autor) => {
+        tabela += `${autor.id}| ${autor.nome}`;
+      });
+
+      tabela += "===================================";
+      return tabela;
+    } catch (error) {
+      return `Erro ao listar os autores: ${(error as Error).message}`;
+    }
+  }
+
+  async buscarPorId(idRaw: string): Promise<string> {
+    const id = Number(idRaw);
+    if (isNaN(id) || idRaw.trim() === "") {
+      return `Erro de formato: O ID que foi fornecido deve ser um número válido`;
+    }
+    try {
+      const autor = await this.autorService.buscarPorId(id);
+      return `Autor foi encontrado: ID:${autor.id}, Nome:${autor.nome}`;
+    } catch (error) {
+      return `Erro: Não foi possivel encontrar este ID ${(error as Error).message}`;
+    }
+  }
+
+  async atualizar(idRaw: string, nomeRaw: string): Promise<string> {
+    const id = Number(idRaw);
+    if (isNaN(id) || idRaw.trim() === "") {
+      return `Erro ao atualizar autor: o ID fornecido deve ser um númro de autor válido`;
+    }
+
+    try {
+      const autorAtualizado = await this.autorService.atualizar(id, {
+        nome: nomeRaw,
+      });
+
+      return `Autor atualizado com sucesso: ID:${autorAtualizado.id} | Nome:${autorAtualizado.nome}`;
+    } catch (error) {
+      return `Erro:Não foi possivel atualizar o autor: ${(error as Error).message}`;
+    }
+  }
+
+  async remover(idRaw: string): Promise<string> {
+    const id = Number(idRaw);
+    if (isNaN(id) || idRaw.trim() === "") {
+      return `Erro ao tentar remover ID de autor fornecido, verificar se o ID fornecido é valido. `;
+    }
+
+    try {
+      await this.autorService.remover(id);
+      return `Autor com o ID ${id} foi removido com sucesso do sistema.`;
+    } catch (error) {
+      return `Erro: Não foi possivel remover este ID ${(error as Error).message}`;
+    }
+  }
+}
---
+++ src/menus/AutorMenu.ts
@@ -0,0 +1,64 @@
+import * as readline from "readline";
+import { AutorController } from "../controllers/AutorController";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const question = (query: string): Promise<string> => {
+  return new Promise((resolve) => rl.question(query, resolve));
+};
+
+export async function mostrarAutorMenu(controller: AutorController) {
+  let executando = true;
+
+  while (executando) {
+    console.log("-----Gerenciamento de autores-----");
+    console.log("1. Cadastrar um autor");
+    console.log("2. Listar todos os Autores");
+    console.log("3. Consultar Autor por ID");
+    console.log("4. Atualizar Autor");
+    console.log("5. Remover Autor");
+    console.log("0. Voltar ao menu principal");
+
+    const opcao = await question("Escolha uma opção: ");
+
+    switch (opcao) {
+      case "1": {
+        const nome = await question("Digite o nome do autor: ");
+        console.log(await controller.criar(nome));
+        break;
+      }
+      case "2": {
+        console.log(await controller.listarTodos());
+        break;
+      }
+      case "3": {
+        const id = await question("Digite o ID do autor para consultar: ");
+        console.log(await controller.buscarPorId(id));
+        break;
+      }
+      case "4": {
+        const id = await question(
+          "Digite o ID do autor que desejas atualizar: ",
+        );
+        const nome = await question("Digite o novo nome do autor: ");
+        console.log(await controller.atualizar(id, nome));
+        break;
+      }
+      case "5": {
+        const id = await question("Digite o ID do autor que deseja remover: ");
+        console.log(await controller.remover(id));
+        break;
+      }
+      case "0": {
+        executando = false;
+        console.log("Voltando ao menu anterior...");
+        break;
+      }
+      default:
+        console.log("Opção inválida!! Tente novamente com uma opção valida.");
+    }
+  }
+}
